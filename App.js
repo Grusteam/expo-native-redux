838bc63cfb880bde9f85946c36cbe5b78f596857
Serge Uniquite <sergeuniquite@gmail.com>
2018-10-30 15:38:52 +0300

    fullscreen, back

--- App.js
+++ App.js
@@ -8,27 +8,24 @@ import rootReducer from './src/reducers/rootReducer.js';
 import { YellowBox } from 'react-native';
 
 const
-  initialState = {
-    count: 0,
-  },
-  store = createStore(
-    rootReducer,
-    initialState,
-  );
-  
+	initialState = {
+		count: 0,
+	},
+	store = createStore(
+		rootReducer,
+		initialState,
+	);
+	
 /* components */
 import App from './src/App.js';
 
-type Props = {};
-
-class ReduxApp extends Component<Props> {
-  render() {
-    const {  } = this.props;
-    
-    return <Provider store={store}>
-      <App/>
-    </Provider>
-  }
+class ReduxApp extends Component {
+	render() {
+		
+		return <Provider store={store}>
+			<App/>
+		</Provider>
+	}
 }
 
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader', 'Remote debugger']);
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {  } from 'redux';
 import {  } from 'react-redux';
+import { WebView } from 'react-native';
 
 import styles from './styles/App.js';
 
@@ -8,13 +9,17 @@ import styles from './styles/App.js';
 import { View } from 'react-native';
 
 /* components */
-import LocalStateApp from './components/LocalStateApp.js';
-import ReduxStateApp from './components/ReduxStateApp.js';
 
 class App extends Component {
 	render() {
-		return <ReduxStateApp test={'+'}/>
+		return <WebView
+			source={{uri: 'https://app.iki.ai'}}
+			style={{  }}
+		/>;
 	}
 }
 
 export default App;
+		
+		
+/* return <ReduxStateApp test={'+++++'}/> */
